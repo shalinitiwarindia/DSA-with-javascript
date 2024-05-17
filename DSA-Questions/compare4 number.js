130da823b70fcb28fedb584d5479e444e5849785
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-05-17 21:18:09 +0530

    Create compare4 number.js

---
+++ DSA-Questions/compare4 number.js
@@ -0,0 +1,8 @@
+function compareFourNumbers(one, two, three, four) {
+    // Write code here
+    if(one>two && three>four){
+        console.log("yes");
+    }else{
+        console.log("No");
+    }
+}
