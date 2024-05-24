eaf1d87a01f76570aec04d21a6e99b9c33a8ca9f
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-05-24 16:56:58 +0530

    Create breakingTheRecord.js

---
+++ DSA-Questions/breakingTheRecord.js
@@ -0,0 +1,13 @@
+function breakingTheRecord(n, m) {
+    //write code here
+   
+        if(m>n){
+            console.log("Broken");
+        }
+        else if(m<n){
+            console.log("Not Yet");
+        }
+        else if(m==n){
+            console.log("Wao");
+        }
+}
