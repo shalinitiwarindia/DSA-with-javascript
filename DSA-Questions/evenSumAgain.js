6f716e525f8695931f98898a8b507fc531907838
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-05-24 16:11:45 +0530

    Create evenSumAgain.js

---
+++ DSA-Questions/evenSumAgain.js
@@ -0,0 +1,10 @@
+function evenSumAgain(num) {
+    // Write code here
+    sum=0;
+    for(let i=1; i<=num; i++){
+        if(i%2==0){
+            sum=sum+i;
+        }
+        console.log(sum);
+    }
+}
