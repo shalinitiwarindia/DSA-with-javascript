70c300b2c022f2e2067c1e0f73416f84371ece4a
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-05-24 16:34:37 +0530

    Create divisionTask.js

---
+++ DSA-Questions/divisionTask.js
@@ -0,0 +1,13 @@
+function divisionTask(N) {
+    // Write code here
+    var x = Math.floor(32/N);
+    if(N==0){
+        console.log(-1)
+    }
+    else if(x == 0){
+        console.log("To Low")
+    }
+    else{
+       console.log(x); 
+    }
+}
