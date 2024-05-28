f5b87e6f3859dde611ba0a1640887a15d6345e3f
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-05-28 21:27:16 +0530

    Create password.js

---
+++ DSA-Questions/password.js
@@ -0,0 +1,10 @@
+ let password = 12345;
+  let newPassword = 4567;
+
+    if(newPassword === password){
+      console.log("Welcome");
+    }
+    else{
+      console.log("Invalid password");
+  }
+  
