29643d10d494701b865c963908e39ee0d132ef17
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-05-30 21:01:34 +0530

    Create local scope vs global scope.js

---
+++ DSA-Questions/local scope vs global scope.js
@@ -0,0 +1,19 @@
+var outside_child = "chintu"; // Global Variables
+
+function sukhbeer_singh(){
+  var sukhbeer_child = "rahul"; // local Variables
+  console.log("My child name is ",sukhbeer_child);
+}
+
+function kalam_singh(){
+  var kalam_child = "rajat"; // local Variables
+  console.log("My child name is ",kalam_child);
+}
+
+function rajendra_singh(){
+  var rajendra_child = "rocky"; // local Variables
+  console.log("My child name is ",rajendra_child);
+}
+
+sukhbeer_singh();
+console.log(outside_child);
