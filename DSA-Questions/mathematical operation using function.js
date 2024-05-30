d169b06e7a18f66f1c688c0889f18c0f90a410e2
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-05-30 21:18:21 +0530

    Create mathematical operation using function.js

---
+++ DSA-Questions/mathematical operation using function.js
@@ -0,0 +1,29 @@
+//Mathematical operation using function
+function addition(){
+  var a = 93;
+  var b = 45;
+  var sum=a+b;
+  console.log(sum);
+}
+function subtraction(){
+  var a = 93;
+  var b = 45;
+  var sub=a-b;
+  console.log(sub);
+}
+function multiplication(){
+  var a = 30;
+  var b = 12;
+  var mul = a*b;
+  console.log(mul);
+}
+function division(){
+  var a = 48;
+  var b = 12;
+  var div = a/b;
+  console.log(div);
+}
+  addition();
+  subtraction();
+  multiplication();
+  division();
