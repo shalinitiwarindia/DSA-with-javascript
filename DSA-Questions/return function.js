f004062bb29c2eca9fa8d27add2859fd52c887d1
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-05-30 22:09:47 +0530

    Create return function.js

---
+++ DSA-Questions/return function.js
@@ -0,0 +1,15 @@
+function addition(a,b)
+{
+  var sum = a+b;
+  return sum;
+}
+function square(a){
+  var sq = a*a;
+  return sq;
+}
+function printVariable(n){
+  console.log(n);
+}
+var result  = addition(20,30);
+var y = square(result);
+printVariable(y);
