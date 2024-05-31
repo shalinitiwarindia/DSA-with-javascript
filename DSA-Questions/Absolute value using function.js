d928bf7ceaa044e2654b9470ce242bbaa81adc0e
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-05-31 20:51:05 +0530

    Create Absolute value using function.js

---
+++ DSA-Questions/Absolute value using function.js
@@ -0,0 +1,19 @@
+function shalini(num1,num2){
+  var ss=(num1-num2)
+  return ss;
+}
+
+function sh(a){
+var hh=Math.abs(a);
+return hh;
+
+}
+
+function printVariable(n){
+  console.log(n);
+}
+
+var s=shalini(12,4);
+var f=sh(s);
+
+printVariable(f);
