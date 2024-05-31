093683d02a5d769f0f504c77cd6027b79854bd44
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-05-31 22:03:32 +0530

    Create Multiply*10 in square of x.js

---
+++ DSA-Questions/Multiply*10 in square of x.js
@@ -0,0 +1,12 @@
+function square(x){
+  return x*x;
+}
+function multiply(num){
+  return num*10;
+}
+function printVariable(n){
+  console.log(n);
+}
+var num = square(5);
+var mul = multiply(num);
+printVariable(mul);
