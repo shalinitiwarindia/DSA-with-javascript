766e2c067088c506c8bad4394512915492f2f20c
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-02 17:23:31 +0530

    Create lastIndexOf.js

---
+++ DSA-Questions/lastIndexOf.js
@@ -0,0 +1,4 @@
+var animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo'];
+console.log(animals.lastIndexOf("Dodo"));
+var people = ["jamna das", "pandu", "kalicharan", "elaichi", "oggy", "pandu"];
+console.log(people.lastIndexOf("jerry"));
