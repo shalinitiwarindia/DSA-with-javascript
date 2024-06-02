3e6d01695c9cf01b4064876b48b6f187cb31c81b
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-02 17:24:15 +0530

    Create indexOf.js

---
+++ DSA-Questions/indexOf.js
@@ -0,0 +1,4 @@
+var animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo'];
+console.log(animals.indexOf("Dodo"));
+var people = ["jamna das", "pandu", "kalicharan", "elaichi", "oggy", "pandu"];
+console.log(people.indexOf("jerry"));
