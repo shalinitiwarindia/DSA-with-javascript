a1dc07eea970243dd6106767b184ed7837722d15
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-02 17:29:30 +0530

    Create inbuilt function indexOf.js

---
+++ DSA-Questions/inbuilt function indexOf.js
@@ -0,0 +1,4 @@
+var paragraph = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+var searchTerm = 'dog';
+var indexOfFirst = paragraph.indexOf(searchTerm);
+console.log(indexOfFirst);
