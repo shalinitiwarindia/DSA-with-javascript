cb367fc6cc24e36d665d1335e30bd2a5a12b1f2d
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-06 22:17:03 +0530

    Create object in object.js

---
+++ DSA-Questions/object in object.js
@@ -0,0 +1,12 @@
+ //object in object
+  var obj={
+    name : "Ram",
+    age : 25,
+    hobbies : ["coding","reading"],
+    address:{
+      city:"Mumbai",
+      pincode:400024,
+    }
+    }
+    obj.movie="karan arjun";
+    console.log(obj);
