6e53f3c621c2d27fecae7045b02fecd873e36ce0
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-06 22:06:16 +0530

    Create delete operation in objects.js

---
+++ DSA-Questions/delete operation in objects.js
@@ -0,0 +1,9 @@
+ //delete operation in objects
+  var obj={
+    name : "Ram",
+    age : 25,
+    hobbies : ["coding","reading"],
+    }
+    obj.movie="karan arjun";
+    delete obj.age;
+  console.log(obj);
