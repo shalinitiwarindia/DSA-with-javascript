b0c548737d7ce4192d2c76e98061708f39ebd8e3
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-07 18:00:55 +0530

    Create Accessing info in array of object.js

---
+++ DSA-Questions/Accessing info in array of object.js
@@ -0,0 +1,10 @@
+var friends = [
+  {name: "Lakhan Singh"  , age : 30  , city : "Delhi"},
+  {name : "Rajma Singh", age : 45, city : "Surat"},
+  {name : "Sudhama", age : 50, city : "Mathura"}
+];
+
+for(var i=0; i<friends.length; i++)
+{
+  console.log(friends[i].name, friends[i].age, friends[i].city) ; 
+}
