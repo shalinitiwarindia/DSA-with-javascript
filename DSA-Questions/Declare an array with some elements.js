a397bb8053335d325ad52593c922e317e857994d
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-10 17:14:32 +0530

    Create Declare an array with some elements.js

---
+++ DSA-Questions/Declare an array with some elements.js
@@ -0,0 +1,11 @@
+// Numbers
+var arr = [2,5,8,9,10,12,15];
+console.log(arr[2]);
+
+// Numbers + Strings
+var arr = [123, "A", "B", 89];
+console.log(arr[2]);
+
+// Numbers + Strings + Arrays
+var arr = [23, "A", [[25,36,42]];
+console.log(arr[0]);
