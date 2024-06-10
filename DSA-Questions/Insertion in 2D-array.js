e68f392c341c08062bbc474a4e717afcf5e8a56e
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-10 16:56:10 +0530

    Create Insertion in 2D-array.js

---
+++ DSA-Questions/Insertion in 2D-array.js
@@ -0,0 +1,9 @@
+//Insertion in 2D-array
+var arr=[];
+var arr1 =['*','*','*'];
+var arr2 = ['%','%'];
+ var arr3=['*','*','*'];         
+ arr.push(arr1);
+arr.push(arr2);
+arr.push(arr3);
+console.log(arr);
