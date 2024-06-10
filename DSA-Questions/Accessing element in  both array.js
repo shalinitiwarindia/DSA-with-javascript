d6f778efe05fe7a8a5dadc8e3c850e6d4f2cf5a3
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-10 16:43:14 +0530

    Create Accessing element in  both array.js

---
+++ DSA-Questions/Accessing element in  both array.js
@@ -0,0 +1,8 @@
+//Accessing element in array
+var arr1 = [1,2,3,4,5];
+var arr2 = [[1,2,3],
+            [4,5,6],
+            [7,8,9],
+           ];
+console.log(arr1[1]);
+console.log(arr2[0][1]);
