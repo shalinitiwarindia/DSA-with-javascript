2fcdcb79766c7968955dfcaf8ec7bdde867a0da3
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-10 16:49:55 +0530

    Create length of array vs 2D array.js

---
+++ DSA-Questions/length of array vs 2D array.js
@@ -0,0 +1,8 @@
+//length of array vs 2D array
+var arr1 = [1,2,3,4,5];
+var arr2 = [[1,2,3],
+            [4,5,6],
+            [7,8,9],
+           ];
+console.log(arr1.length);
+console.log(arr2.length);
