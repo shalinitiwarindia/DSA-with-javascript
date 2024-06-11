d257122a8059b88367a9a18cc4408de46281d468
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-11 16:49:30 +0530

    Create even Sum in Rows.js

---
+++ DSA-Questions /even Sum in Rows.js
@@ -0,0 +1,19 @@
+function evenSumRows(N,M,arr){
+    //write code here
+    let rowSums=[];
+    for(let i=0;i<arr.length;i++){
+        let sum=0;
+        for(let j=0;j<arr[i].length;j++){
+            if(arr[i][j]%2===0){
+             sum+=arr[i][j];   
+            }
+            
+          }
+          rowSums.push(sum);
+    }
+    console.log(rowSums.join(" "));
+}
+//output
+//2
+//10
+//8
