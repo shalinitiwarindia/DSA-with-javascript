aea84ac534422621c196edf577206938c4246241
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-16 21:54:28 +0530

    Create subString.js

---
+++ DSA-Questions/subString.js
@@ -0,0 +1,10 @@
+function subString(str){
+  for(var i=0;i<name.length;i++){
+    var bag="";
+    for(var j=i;j<name.length;j++){
+      bag=bag+name[j];
+      console.log(bag);
+    }
+  }
+}
+subString("masai");
