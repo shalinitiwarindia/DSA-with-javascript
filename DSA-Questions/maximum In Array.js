9859dfffebe397d1760e866ad5379ba5b0df839a
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-17 22:00:41 +0530

    Create maximum In Array.js

---
+++ DSA-Questions/maximum In Array.js
@@ -0,0 +1,10 @@
+function maximumInArray(N,arr){
+    //write code 
+    let max=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    console.log(max);
+}
