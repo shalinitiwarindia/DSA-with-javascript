14e0aba3458b2d0592b212856bc2009821d35ea2
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-17 21:17:00 +0530

    Create odd Array.js

---
+++ DSA-Questions/odd Array.js
@@ -0,0 +1,9 @@
+//OJ solution
+function oddArray(n, arr){
+    //write code here
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]%2!==0){
+            console.log(arr[i]);
+        }
+    }
+}
