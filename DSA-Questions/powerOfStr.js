b2cd7c57440613386122b69c64cbcd07f3a37731
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-19 21:58:13 +0530

    Create powerOfStr.js

---
+++ DSA-Questions/powerOfStr.js
@@ -0,0 +1,19 @@
+function powerOfStr(N, str){
+  let vowelCount=0;
+  let consonant=0;
+ 
+        var x = str;
+        for(var k=0;k<x.length;k++){
+            if(x[k]=="a" || 
+            x[k]=="e" || 
+            x[k]=="i" ||
+            x[k]=="o" || 
+            x[k]=="u"){
+                vowelCount++;
+            }
+            else{
+                consonant++;
+            }
+  }
+        console.log(5*vowelCount+2*consonant);
+}
