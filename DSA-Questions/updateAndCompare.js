69549f0d5ceecec81b713637a554821515b8a1aa
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-20 14:02:57 +0530

    Create updateAndCompare.js

---
+++ DSA-Questions/updateAndCompare.js
@@ -0,0 +1,14 @@
+function updateAndCompare(num1, num2, num3) {
+    // Write code here 
+    
+    if(num1>num2){
+        console.log("true");
+    }else{
+        console.log("false");
+    }
+    if(num1+num3>num2){
+        console.log("true");
+    }else{
+        console.log("false");
+    }
+}
