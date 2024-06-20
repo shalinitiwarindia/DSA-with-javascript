5af162e3f58f5db7550eab2ca01748fb1917f8a4
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-20 14:57:00 +0530

    Create theAshes.js

---
+++ DSA-Questions/theAshes.js
@@ -0,0 +1,13 @@
+function theAshes(Australia, England) {
+    //Write code here
+    if(Australia>England){
+        console.log("Australia");
+    }
+    else if(Australia<England){
+        console.log("England");
+    }
+    else{
+        console.log("Tie");
+    }
+    
+}
