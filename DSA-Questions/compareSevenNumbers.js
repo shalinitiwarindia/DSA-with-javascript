b7a30d62ba37d48fafab4ec278f67fc49036a134
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-20 14:15:31 +0530

    Create compareSevenNumbers.js

---
+++ DSA-Questions/compareSevenNumbers.js
@@ -0,0 +1,10 @@
+function compareSevenNumbers(a,b,c,d,e,f,g) {
+    // Write code here
+    let sum1=(a+b)*c;
+    let sum2=(d+e+f+g);
+    if(sum1>sum2){
+        console.log("true");
+    }else{
+        console.log("false");
+    }
+}
