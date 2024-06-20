0521ede1b784c026baf06214b45089e53387cb5e
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-20 15:03:22 +0530

    Create enoughFuelConsumption.js

---
+++ DSA-Questions/enoughFuelConsumption.js
@@ -0,0 +1,8 @@
+function enoughFuelConsumption(fuel, distance) {
+    let required=fuel*distance;
+    if(required>50){
+        console.log("Enough");
+    }else{
+        console.log("Go On");
+    }
+}
