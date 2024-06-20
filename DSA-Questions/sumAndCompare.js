0b2e957292cccaf75bd39c19905629cbdcb5dff6
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-20 14:11:40 +0530

    Create sumAndCompare.js

---
+++ DSA-Questions/sumAndCompare.js
@@ -0,0 +1,10 @@
+function sumAndCompare(one, two, three, four, five) {
+    // Write code here
+    let sum1=one+two+three;
+    let sum2=four+five;
+    if(sum1>sum2){
+        console.log("true");
+    }else{
+        console.log("false");
+    }
+}
