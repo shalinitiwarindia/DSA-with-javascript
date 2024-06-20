d1916d1ef73ac16d084216450687b953a495c077
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-20 15:38:15 +0530

    Create twoSumsOneNumberII.js

---
+++ DSA-Questions/twoSumsOneNumberII.js
@@ -0,0 +1,10 @@
+function twoSumsOneNumberII(one, two, three, four, five) {
+    // Write code here
+    let sum1=one+two;
+    let sum2=three+four;
+    if(sum1>five || sum2>five){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
+}
