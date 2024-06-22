01cca13e9d40bf60519478596c00a69d7635b794
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-22 09:04:13 +0530

    Create Basic of object.js

---
+++ DSA-Questions/Basic of object.js
@@ -0,0 +1,9 @@
+//creating object and accessing properties
+var student={
+  name:"Rahul",
+  age:20,
+  gendar:"male",
+};
+  console.log(student);
+console.log(student.name); //dot notation
+console.log(student["name"]); //bracket notation
