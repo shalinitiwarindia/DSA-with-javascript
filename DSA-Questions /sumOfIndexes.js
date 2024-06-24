df483bf38946e8a50cec7c4a0a86df4fd8aed5f8
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-24 20:48:04 +0530

    Create sumOfIndexes.js

---
+++ DSA-Questions /sumOfIndexes.js
@@ -0,0 +1,8 @@
+function sumOfIndexes(N,M,arr){
+    //write code here 
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length-1;j++){
+            console.log(i+j);
+        }
+    }
+}
