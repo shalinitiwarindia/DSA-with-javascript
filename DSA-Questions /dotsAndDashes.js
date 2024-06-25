7b6dcea948b51303893bafb794f93a80487fefd8
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-06-25 13:09:00 +0530

    Create dotsAndDashes.js

---
+++ DSA-Questions /dotsAndDashes.js
@@ -0,0 +1,13 @@
+function dotsAndDashes(N) {
+for(let i=1; i<=N; i++){
+       var bag="";
+       for(var j=1; j<=N;j++){
+          bag=bag+"*";
+       }
+       if(i%2!==0){
+           console.log("~");
+       }else{
+           console.log(bag);
+       }
+     }
+}
