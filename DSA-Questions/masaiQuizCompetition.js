3a7d5b223d7c10c166379e347721bc8f8530d9b7
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-07-03 21:20:55 +0530

    Create  masaiQuizCompetition.js

---
+++ DSA-Questions/masaiQuizCompetition.js
@@ -0,0 +1,10 @@
+function masaiQuizCompetition(n,arr){
+	//write your code here
+	let count=1;
+	for(let i=0;i<arr.length;i++){
+	    if(arr[i]===0){
+	        count++;
+	    }
+	}
+	console.log(count);
+}
