c9548de571f0b51eac55a9cc04da2c6fa0b5c467
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-10-20 15:49:53 +0530

    Create oddSumColumns.js

---
+++ Multi Dimensional Arrays/oddSumColumns.js
@@ -0,0 +1,15 @@
+function oddSumColumns(N,M,arr){
+    //write code here
+    let oddSums=[];
+    for(let i=0;i<arr.length;i++){
+        let sum=0;
+        for(let j=0;j<arr[i].length;j++){
+            if(arr[j][i]%2!==0){
+              sum+=arr[j][i];  
+            }
+            
+          }
+          oddSums.push(sum);
+    }
+    console.log(oddSums.join(" "));
+}
