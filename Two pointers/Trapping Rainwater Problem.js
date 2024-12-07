7bbe1972834a39f78e316878d28d2eec5b9df2d0
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-12-07 20:43:57 +0530

    Create Trapping Rainwater Problem.js

---
+++ Two pointers/Trapping Rainwater Problem.js
@@ -0,0 +1,26 @@
+function trapRainWater(height) {
+    let left = 0, right = height.length - 1;
+    let leftMax = 0, rightMax = 0, water = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                water += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                water += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+    return water;
+}
+
+// Example Usage
+console.log(trapRainWater([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])); // Output: 6
