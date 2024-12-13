cc36244c3f04f602d5a357c7ed4fd31d9a1ef8a7
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2024-12-13 21:58:36 +0530

    Update Count Occurrences of an Element.js

--- Binary Search/Count Occurrences of an Element.js
+++ Binary Search/Count Occurrences of an Element.js
@@ -2,7 +2,3 @@ function countOccurrences(arr, target) {
   const first = firstOccurrence(arr, target);
   if (first === -1) return 0; // Target not found
 
-  const last = lastOccurrence(arr, target);
-  return last - first + 1;
-}
-
