9dae37df7402cfbaf11b2fb7476ce3ad2bc8a3ec
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-02-15 23:13:42 +0530

    Update Add Two Numbers Represented by Linked Lists.js

--- Linked List/Add Two Numbers Represented by Linked Lists.js
+++ Linked List/Add Two Numbers Represented by Linked Lists.js
@@ -3,14 +3,4 @@ function addTwoNumbers(l1, l2) {
   let current = dummy;
   let carry = 0;
 
-  while (l1 || l2 || carry) {
-    let sum = (l1 ? l1.value : 0) + (l2 ? l2.value : 0) + carry;
-    carry = Math.floor(sum / 10);
-    current.next = new ListNode(sum % 10);
-    current = current.next;
-    l1 = l1 ? l1.next : null;
-    l2 = l2 ? l2.next : null;
-  }
-
-  return dummy.next;
-}
+  wh
