3e4286a75eecb0ede12cfc16e3e52d45ed144621
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-02-25 23:49:52 +0530

    Update A Football Tournament.js

--- DSA-Questions /A Football Tournament.js
+++ DSA-Questions /A Football Tournament.js
@@ -1,3 +1,31 @@
+function generateKeyValuePair(array) {
+    const keyValuePair = {};
+    array.forEach(element => {
+        if (keyValuePair[element]) {
+            keyValuePair[element]++;
+        } else {
+            keyValuePair[element] = 1;
+        }
+    });
+    return keyValuePair;
+}
 
+function aFootballTournament(array) {
+    const keyValuePair = generateKeyValuePair(array);
+    let max = 0;
+    let ans = '';
 
-// 
+    for (const key in keyValuePair) {
+        if (keyValuePair[key] > max) {
+            max = keyValuePair[key];
+            ans = key;
+        }
+    }
+
+    return ans;
+}
+
+// Example usage:
+const teams = ["TeamA", "TeamB", "TeamA", "TeamC", "TeamB", "TeamA"];
+const mostFrequentTeam = aFootballTournament(teams);
+console.log(mostFrequentTeam); // Output: TeamA
