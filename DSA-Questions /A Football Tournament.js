7e19ddff6e48f7c14e35568d50ef00b2de026ccb
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-08-15 17:06:36 +0530

    Update A Football Tournament.js

--- DSA-Questions /A Football Tournament.js
+++ DSA-Questions /A Football Tournament.js
@@ -2,10 +2,6 @@ function generateKeyValuePair(array) {
     const keyValuePair = {};
     array.forEach(element => {
         if (keyValuePair[element]) {
-            keyValuePair[element]++;
-        } else {
-            keyValuePair[element] = 1;
-        }
-
+       
        
 
